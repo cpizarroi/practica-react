b6ed69237867b6b544ba313baf9a5f695afa4f15
Camila Pizarro <c.pizarroibaceta@gmail.com>
2024-06-13 18:34:39 -0400

    practica

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Title } from './Components/Title'
+
+function App() {
+  return(
+    <div>
+      <Title></Title>
+    </div>
+  );
+
+  
+}
+
+export default App
